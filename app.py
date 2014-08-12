28c45de470c1da27f77a9f3e55d13120b1077742
Nicholas Lee <csudcy@gmail.com>
2014-08-13 00:43:10 +0100

    Added lots of setup code

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
 import os
+
 from flask import Flask
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.heroku import Heroku
+from flask.ext.sqlalchemy import SQLAlchemy
+
+##################################################
+#                    Setup
+##################################################
 
 app = Flask(__name__)
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+admin = Admin(app)
+
+##################################################
+#                    Models
+##################################################
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+admin.add_view(ModelView(User, db.session))
+
+##################################################
+#                    Routes
+##################################################
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
+##################################################
+#                    Main
+##################################################
+
 if __name__ == '__main__':
+    # Ensure all our tables are created
+    db.create_all()
+
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
