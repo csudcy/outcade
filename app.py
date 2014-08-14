d8b345caa247203dbb8061dd529291edf56ac089
Nicholas Lee <csudcy@gmail.com>
2014-08-14 22:13:33 +0100

    Added alembic

--- app.py
+++ app.py
@@ -287,6 +287,10 @@ def production():
     # Turn off debug on live...
     app.debug = False
 
+    # Upgrade the DB
+    from flask.ext.migrate import upgrade
+    upgrade()
+
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
